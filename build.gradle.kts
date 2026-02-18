2b77977538e8356e71ab2e484ad8758626022876
Aman Kumar Maurya <amankrmj@gmail.com>
2026-02-18 10:51:46 +0530

    Add Lombok annotation processor and Jackson databind dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     implementation("com.google.guava:guava:33.5.0-jre")
     implementation("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    implementation("tools.jackson.core:jackson-databind:3.0.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
