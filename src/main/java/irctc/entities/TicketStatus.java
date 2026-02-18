5393d8eb0d947f6558bbb9a09804c97f765c9e35
Aman Kumar Maurya <amankrmj@gmail.com>
2026-02-18 10:52:37 +0530

    Add enums for TicketStatus and TicketType

---
+++ src/main/java/irctc/entities/TicketStatus.java
@@ -0,0 +1,8 @@
+package irctc.entities;
+
+public enum TicketStatus {
+    BOOKED,
+    CANCELLED,
+    WAITING_LIST,
+    CONFIRMATION_PENDING
+}
---
+++ src/main/java/irctc/entities/TicketType.java
@@ -0,0 +1,9 @@
+package irctc.entities;
+
+public enum TicketType {
+    SLEEPER,
+    THIRD_AC,
+    SECOND_AC,
+    FIRST_AC,
+    GENERAL
+}
